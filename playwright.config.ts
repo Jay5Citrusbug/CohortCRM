569f6b3678c3ed8eb93732312b913b043890632f
unknown <jay5.citrusbug@gmail.com>
2025-10-28 17:15:05 +0530

    Pipeline setup2

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,8 +27,9 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-      ['html', { outputFolder: 'playwright-report', open: 'never', title: 'Cohort CRM - Automation Report' }]
-
+      ['html', { outputFolder: 'playwright-report', open: 'never', title: 'Cohort CRM - Automation Report' }],
+      ['list'],
+      ['json', { outputFile: 'playwright-report/report.json' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   
