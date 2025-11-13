39001f8de84255bd52c131fbdd15b5c7f8a96696
unknown <jay5.citrusbug@gmail.com>
2025-11-13 15:31:33 +0530

    Resolved issue3

--- tests/Companies/Companies.spec.ts
+++ tests/Companies/Companies.spec.ts
@@ -2,7 +2,7 @@
 import { CompaniesPage } from '../../Pages/Companies.page';
 
 
-  test.describe.only('Contact Screen Tests', () => {
+  test.describe('Contact Screen Tests', () => {
     let context: BrowserContext;
     let page: Page;
     let companiesPage: CompaniesPage;
