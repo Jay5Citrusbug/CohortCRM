4861594af0ea09e8bf98c474beaba65137465339
unknown <jay5.citrusbug@gmail.com>
2025-12-10 11:58:37 +0530

    Fix bug for NPW status

--- tests/Pipeline/Loan_Status.spec.ts
+++ tests/Pipeline/Loan_Status.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { PipeLinePage } from '../../Pages/Pipelinepage';
 import { LoanStatus } from '../../Pages/Loan_Status'
 
-test.describe.only('Pipeline loan Status', () => {
+test.describe('Pipeline loan Status', () => {
   let loanStatusPage: LoanStatus;
   let pipeLinePage: PipeLinePage;
 
