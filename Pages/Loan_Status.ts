de566c37f662d00b9b29330f73bcd67fc03d1105
unknown <jay5.citrusbug@gmail.com>
2025-12-15 10:45:05 +0530

    Order

--- Pages/Loan_Status.ts
+++ Pages/Loan_Status.ts
@@ -199,7 +199,7 @@ export class LoanStatus {
     await statusDropdown.click();
 
     // Select "Underwriting Process"
-    const underwritingOption = this.page.getByText('Underwriting Process');
+    const underwritingOption = this.page.getByText('Underwriting Process').first();
     await expect(underwritingOption).toBeVisible();
     await underwritingOption.click();
 
